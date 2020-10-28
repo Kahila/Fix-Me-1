b2a714b3ee1f32ff2e16e6e470431bc20ecdf913
YaQwaQwa <lebus23@gmail.com>
2020-10-28 12:37:23 +0200

    first

---
+++ fixme-router/src/main/java/com/lnzimand/App.java
@@ -0,0 +1,12 @@
+package com.lnzimand;
+
+import com.lnzimand.controller.RouterController;
+
+/**
+ * @author yaqwaqwa
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
